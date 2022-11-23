158fd1cf803c92fcd580effb6f92de2ddaf09bbc
Florian Erwerth <erwerthflorian@outlook.de>
2022-11-23 10:57:15 +0100

    fix: import

--- src/components/Layout/container/container.tsx
+++ src/components/Layout/container/container.tsx
@@ -1,5 +1,5 @@
 import "./styles.scss";
-import {WithChildren} from "../../types";
+import {WithChildren} from "../../../types";
 
 export const Container = ({children}: WithChildren) => {
    return <div className={"container"}>{children}</div>
